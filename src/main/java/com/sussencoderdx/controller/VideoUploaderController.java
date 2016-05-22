311ce34e709af68913688b20bd0ee67be3356b2a
Vinícius Candelária <viniciuscandelaria@gmail.com>
2016-05-21 23:20:04 -0300

    Retirada de código de teste

--- src/main/java/com/sussencoderdx/controller/VideoUploaderController.java
+++ src/main/java/com/sussencoderdx/controller/VideoUploaderController.java
@@ -28,35 +28,12 @@ public class VideoUploaderController {
 		String home = System.getProperty("user.home");
 		String uploadedFileLocation = home+"/Downloads" + details.getFileName();
 
-		// save it
-		writeToFile(uploadedVideo, uploadedFileLocation);
-
+		// Salvar 
+		
 		String output = "Your memory was collected : " + uploadedFileLocation;
 
 		return Response.status(200).entity(output).build();
 
 	}
-
-	// save uploaded file to new location
-	private void writeToFile(InputStream uploadedInputStream,
-		String uploadedFileLocation) {
-
-		try {
-			OutputStream out = new FileOutputStream(new File(
-					uploadedFileLocation));
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			out = new FileOutputStream(new File(uploadedFileLocation));
-			while ((read = uploadedInputStream.read(bytes)) != -1) {
-				out.write(bytes, 0, read);
-			}
-			out.flush();
-			out.close();
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-
-	}
+	
 }
